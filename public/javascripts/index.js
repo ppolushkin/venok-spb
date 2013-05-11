ed4bfe56d261f7c82e52cfdca599f1ea276063cb
Pavel Polushkin <ppolushkin@enkata.com>
2013-05-12 03:54:00 +0400

    menu highlighting

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,22 +1,43 @@
 $(document).ready(function () {
+    highlightAdminMenu();
     highlightMenu();
+    highlightNavigationMenu();
     logout();
 });
 
+function highlightAdminMenu() {
+    var str = location.href.toLowerCase();
+    $("#admin ul li.on").removeClass("on");
+    $("#admin ul li a").each(function () {
+        if (str == this.href.toLowerCase()) {
+            $(this).parent().addClass("on");
+        }
+    });
+}
+
 function highlightMenu() {
     var str = location.href.toLowerCase();
-    var is_root = location.pathname == "/";
-    $("li.on").removeClass("on");
+    $("#menu li.on").removeClass("on");
 
-    if (is_root) {
-        $("li.root").addClass("on");
-    } else {
-        $("#menu li a").each(function () {
-            if (str == this.href.toLowerCase()) {
+    $("#menu li a").each(function () {
+        if (this.pathname == '/') {
+            if(location.pathname == '/') {
                 $(this).parent().addClass("on");
             }
-        });
-    }
+        } else if (str.indexOf(this.href.toLowerCase()) > -1) {
+            $(this).parent().addClass("on");
+        }
+    });
+}
+
+function highlightNavigationMenu() {
+    var str = location.href.toLowerCase();
+    $("#navigation ul li.on").removeClass("on");
+    $("#navigation ul li a").each(function () {
+        if (str == this.href.toLowerCase()) {
+            $(this).parent().addClass("on");
+        }
+    });
 }
 
 function logout() {
