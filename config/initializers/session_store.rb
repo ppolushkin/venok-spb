c0b84ab08b3a431506cada053a718e647a609f18
Pavel Polushkin <ppolushkin@enkata.com>
2013-05-15 16:48:58 +0400

    some configurations change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Obelisk::Application.config.session_store :cookie_store, :key => '_obelisk_session'
+Obelisk::Application.config.session_store :cookie_store, :key => '_venok_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
